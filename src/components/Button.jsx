cd433055906bfa676ff0200442fd72c3f6201008
EdgarGrigoryan1996 <footballistna@gmail.com>
2025-04-09 02:25:50 +0400

    button change size

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,6 +5,8 @@ function Button(props) {
     backgroundColor: props.bg,
     color: props.color,
     fontSize: "1.2rem",
+    width: "300px",
+    height: "60px",
   };
   return <button style={styles}>{props.title}</button>;
 }
